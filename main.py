18722f1e3a050aca710fa3da0fd4598efe973078
user <test@mail.ru>
2023-12-16 12:55:53 +0300

    added print

--- main.py
+++ main.py
@@ -821,4 +821,118 @@
 
 # print("Вносим изменения")
 
-print("Склонированный репозиторий")
+# print("Склонированный репозиторий")
+
+# Множества (set)
+
+# s = {"banana", "apple", "orange", "banana", "apple"}
+# print(s)
+
+# c = ["red", "blue", "green", "red"]
+# a = set(c)
+# print(a, type(a))
+
+# mas = [1, 2, 3, 2, 3, 4, 4, 5]
+# s = {x for x in mas}
+# print(s)
+
+#
+# def to_set(elem):
+#     return set(elem), len(elem)
+#
+#
+# print(to_set("я обычная строка"))
+# print(to_set([4, 5, 4, 6, 2, 9, 11, 3, 4, 2]))
+
+
+# def to_set(element):
+#     st = set(element)
+#     return st, len(st)
+#
+#
+# print(to_set('я обычная строка'))
+# print(to_set([4, 5, 4, 6, 2, 9, 11, 3, 4, 2]))
+
+# t = {"red", "green", "blue"}
+# for i in t:
+#     print(i)
+
+# r = ['ab_1', 'ac_2', 'bc_1', 'bc_2']
+# # a = ["A" + i[1:] if i[0] == 'a' else 'B' + i[1:] for i in r]
+# a = ["A" + i[1:] if i[0] == 'a' else 'B' + i[1:] for i in r if i[1] == 'c']
+# print(a)
+
+# a = {'Tom', 'Bob', 'Alice'}
+# print(a)
+# a.add('Ann')
+# print(a)
+# a.remove("Ann")  # при обращении к несуществующему элементу - ошибка KeyError
+# print(a)
+# user = "Ann"
+# if user in a:
+#     a.remove(user)
+# print(a)
+
+
+# a.discard("Ann")
+# n = a.pop()
+# print(n)
+# a.pop()
+# a.clear()
+# print(a)
+# print(n)
+
+
+# a = {0, 1, 2, 3}
+# b = {4, 3, 2, 1}
+# # # c = a | b
+# # a |= b
+# # print(a)
+#
+# a ^= b
+# print(a)
+
+# s1 = {1, 2}
+# s2 = {3}
+# s3 = {4, 5}
+# s4 = {3, 2, 6}
+# s5 = {6}
+# s6 = {7,8}
+# s7 = {9, 8}
+#
+# # s = s1.union(s2, s3, s4, s5, s6, s7)
+# s = s1 | s2 | s3 | s4 | s5 | s6 | s7
+# print(s)
+
+# s1 = "Hello"
+# s2 = "How are you"
+# a = set(s1) & set(s2)
+# print(a)
+# for i in a:
+#     print(i, end=" ")
+
+# drawing = {"Марина", "Женя", "Света"}
+# music = {"Костя", "Женя", "Илья"}
+#
+# one_hobby = drawing ^ music
+# print("Один кружок: ", one_hobby)
+#
+# both_hobbies = drawing & music
+# print("Оба кружка", both_hobbies)
+#
+# drawing -= both_hobbies
+# print(drawing)
+
+# a = {0, 1, 2, 3, 4}
+# b = {3, 2, 1}
+# print(a <= b)
+# print(a >= b)
+
+
+# Тип frozenset
+s = frozenset([1, 2, 3, 4, 5])
+print(s)
+print(type(s))
+a = frozenset({"hello", "world"})
+
+# Словарь
