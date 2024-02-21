85b2b4b2efc18a46058b2d994fa8e24e73b4e9e3
user <test@mail.ru>
2024-02-21 22:23:12 +0300

    new commit

--- main.py
+++ main.py
@@ -2426,21 +2426,325 @@
 
 
 # 1
-class Car:
-    def __init__(self, model_name, year, company, power, color, price):
-        self.model_name = model_name
-        self.year = year
-        self.company = company
-        self.power = power
-        self.color = color
-        self.price = price
+# class Car:
+#     def __init__(self, model_name, year, company, power, color, price):
+#         self.model_name = model_name
+#         self.year = year
+#         self.company = company
+#         self.power = power
+#         self.color = color
+#         self.price = price
+#
+#     def print_info(self):
+#         print(" Данные автомобиля ".center(40, '*'))
+#         print(f'Название модели: {self.model_name}\nГода выпуска: {self.year}\nПроизводитель: {self.company}\n'
+#               f'Мощность двигателя: {self.power} л.с.\nЦвет машины: {self.color}\nЦена: {self.price}')
+#         print("=" * 40)
+#
+#
+# p1 = Car('Х7 М50i', '2021', 'BMW', '530', 'white', '10790000')
+# p1.print_info()
+
+
+# 17/2/2024
+
+# class Point:
+#     __slots__ = ('__x', '__y')
+#     def __init__(self, x, y):
+#         self.__x = self.__y = 0
+#         if Point.__check_value(x) and Point.__check_value(y):
+#             self.__x = x
+#             self.__y = y
+#
+#     def __check_value(c):
+#         if isinstance(c, int) or isinstance(c, float):
+#             return True
+#         return False
+#
+#     def get_cord(self):
+#         return self.__x, self.__y
+#
+#     def set_cord(self, x, y):
+#         if Point.__check_value(x) and Point.__check_value(y):
+#             self.__x = x
+#             self.__y = y
+#         else:
+#             print("Координаты должны быть числами")
+#
+#     def get_x(self):
+#         return self.__x
+#
+#     def get_y(self):
+#         return self.__y
+#
+#     def set_x(self, x):
+#         if Point.__check_value(x):
+#             self.__x = x
+#         else:
+#             print("Координата", x, "должна быть числом")
+#
+#     def set_y(self, y):
+#
+#         if Point.__check_value(y):
+#             self.__x = y
+#         else:
+#             print("Координата", y, "должна быть числом")
+#
+#
+# p1 = Point(5, 10)
+# p1.set_x(20.6)
+# p1.set_y("aaa")
+# # print(p1.get_cord())
+# # p1.set_cord("6", 7)
+# # print(p1.__x, p1.__y)
+# # p1.x = 100
+# # p1.y = 'abc'
+# # print(p1.x, p1.y)
+# print(p1.get_x())
+# print(p1.get_y())
+# print(p1.__dict__)
+
+# class Point:
+#     def __init__(self, x, y):
+#         self.__x = x
+#         self.__y = y
+#
+#     def __get_x(self):
+#         return self.__x
+#
+#     def __get_y(self):
+#         return self.__y
+#
+#     def __set_x(self, x):
+#         self.__x = x
+#
+#     def __set_y(self, y):
+#         self.__y = y
+#
+#     x = property(__get_x, __set_x)
+#
+#
+# p1 = Point(5, 10)
+# p1.x = 100
+# print(p1.__dict__)
+
+# class Point:
+#     def __init__(self, x, y):
+#         self.__x = x
+#         self.__y = y
+#
+#     def __get_x(self):
+#         return self.__x
+#
+#     def __get_y(self):
+#         return self.__y
+#
+#     def __set_x(self, x):
+#         self.__x = x
+#
+#     def __set_y(self, y):
+#         self.__y = y
+#
+#     def __del_x(self):
+#         print("Удаление свойства")
+#     x = property(__get_x, __set_x)
+#
+#
+# p1 = Point(5, 10)
+# p1.x = 100
+# print(p1.__dict__)
+
+
+# class Point:
+#     def __init__(self, x, y):
+#         self.__x = x
+#         self.__y = y
+#
+#     @property
+#     def x(self):
+#         return self.__x
+#
+#     @x.setter
+#     def x(self, x):
+#         if not isinstance(x, (int, float)):
+#             raise TypeError("Устанавливаемое значение должно быть числом")
+#         self.__x = x
+#
+#     @x.deleter
+#     def x(self):
+#         print("Удаление свойства")
+#         del self.__x
+#
+#     def __get_y(self):
+#         return self.__y
+#
+#     def __set_y(self, y):
+#         self.__y = y
+#
+#
+# p1 = Point(5, 10)
+# p1.x = 100
+# print(p1.x)
+
+
+# class KgToPounds:
+#     def __init__(self, kg):
+#         self.__kg = kg
+#
+#     @property
+#     def kg(self):
+#         return self.__kg
+#
+#     @kg.setter
+#     def kg(self, new_kg):
+#         if isinstance(new_kg, (int, float)):
+#             self.__kg = new_kg
+#         else:
+#             print("Килограммы задаются только числами")
+#
+#     def to_pounds(self):
+#         return self.__kg * 2.205
+#
+#     def print_data(self):
+#         print(self.kg, "кг => ", end='')
+#         print(self.to_pounds(), "фунтов")
+#
+#
+# weight = KgToPounds(12)
+# weight.print_data()
+
+
+# class Point:
+#     __count = 0
+#
+#     def __init__(self, x=0, y=0):
+#         self.x = x
+#         self.y = y
+#         Point.__count += 1
+#
+#     @staticmethod
+#     def get_count():
+#         return Point.__count
+#
+#
+# p1 = Point()
+# p2 = Point()
+# p3 = Point()
+#
+# print(Point.get_count())
+
+
+# class Change:
+#     @staticmethod
+#     def inc(x):
+#         return x + 1
+#
+#     @staticmethod
+#     def dex(x):
+#         return x - 1
+#
+#
+# print(Change.inc(10), Change.dex(10))
+
+
+# class Tools:
+#     @staticmethod
+#     def maximum(my_list):
+#         return max(my_list)
+#
+#     @staticmethod
+#     def minimum(my_list):
+#         return min(my_list)
+#
+#     @staticmethod
+#     def middle(my_list):
+#         return sum(my_list) / len(my_list)  # среднее арифметическое
+#
+#     # @staticmethod
+#     # def factorial(number):
+#     #     if number == 1:
+#     #         return 1
+#     #     else:
+#     #         return Tools.factorial(number - 1) * number
+#     @staticmethod
+#     def factorial(number):
+#         fact = 1
+#         for i in range(number):
+#             fact *= i
+#         return fact
+#
+#
+# print(f'Максимальное число: {Tools.maximum([3, 5, 7, 9])}')
+# print(f'Минимум число: {Tools.minimum([3, 5, 7, 9])}')
+# print(f'Среднее арифметическое: {Tools.middle([3, 5, 7, 9])}')
+# print(f'Факториал числа {5}: {Tools.factorial(5)}')
+
+# from math import sqrt
+#
+#
+# class Square:
+#     count = 0
+#
+#     @staticmethod
+#     def square_triangle1(a, b, c):
+#         Square.count += 1
+#         p = (a + b + c) / 2
+#         return sqrt(p * (p - a) * (p - b) * (p - c))
+#
+#     @staticmethod
+#     def square_triangle2(a, b):
+#         Square.count += 1
+#         return 0.5 * a * b
+#
+#     @staticmethod
+#     def square_area(a):
+#         Square.count += 1
+#         return a * a
+#
+#     @staticmethod
+#     def square_rectangle(a, b):
+#         Square.count += 1
+#         return a * b
+#
+#     @staticmethod
+#     def get_count():
+#         return Square.count
+#
+#
+# print('Площадь треугольника по формуле Герона:', Square.square_triangle1(3, 4, 5))
+# print('Площадь треугольника через основание и высоту:', Square.square_triangle2(6, 7))
+# print('Площадь квадрата:', Square.square_area(7))
+# print('Площадь прямоугольника:', Square.square_rectangle(2, 6))
+# print('Количество подсчетов площади:', Square.get_count())
+
+
+# 1
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
 
     def print_info(self):
-        print(" Данные автомобиля ".center(40, '*'))
-        print(f'Название модели: {self.model_name}\nГода выпуска: {self.year}\nПроизводитель: {self.company}\n'
-              f'Мощность двигателя: {self.power} л.с.\nЦвет машины: {self.color}\nЦена: {self.price}')
-        print("=" * 40)
+        print(f'Длина прямоугольника: {self.length}')
+        print(f'Ширина прямоугольника: {self.width}')
+
+    def square(self):
+        print(f'Площадь прямоугольника: {self.length * self.width}')
+
+    def perimeter(self):
+        print(f'Периметр прямоугольника: {self.length * 2 + self.width * 2}')
+
+    def diagonal(self):
+        print(f'Диагональ прямоугольника: {round((self.length ** 2 + self.width ** 2) ** 0.5, 2)}')
+
+    def draw_rectangle(self):
+        for i in range(self.length):
+            print('*' * self.width)
 
 
-p1 = Car('Х7 М50i', '2021', 'BMW', '530', 'white', '10790000')
+p1 = Rectangle(3, 9)
 p1.print_info()
+p1.square()
+p1.perimeter()
+p1.diagonal()
+p1.draw_rectangle()
