3a6db055e8d2c32691d453f9e57d855946dcfb7d
illustratio <test@gmail.com>
2024-04-06 15:37:44 +0300

    pj65_hw

--- pj65/homework.py
+++ pj65/homework.py
@@ -3,4 +3,4 @@ import csv
 with open('data2.csv') as f:
     file_reader = csv.reader(f, delimiter=';')
     for row in file_reader:
-        print([row[0], row[1], row[2], row[3]] )
+        print([row[0], row[1], row[2], row[3]])
