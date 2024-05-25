1d2d07dd632c77a5fce06686aeb2f16ee95e8e52
bugweaver <bugweaver@example.com>
2024-05-25 20:57:07 +0300

    pj76

--- projects/pj76/flsite/flsite.py
+++ projects/pj76/flsite/flsite.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, flash, session, redirect
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = "fjnk35983yt23fgpq38t483"
 menu = [
     {"name": "Главное", "url": "index"},
     {"name": "О нас", "url": "about"},
@@ -13,12 +13,51 @@ menu = [
 @app.route('/')
 @app.route('/index')
 def index():
+    print(url_for('index'))
     return render_template("index.html", title="Главная", menu=menu)
 
 
 @app.route('/about')
 def about():
+    print(url_for('about'))
     return render_template('about.html', title="О нас", menu=menu)
+
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == "POST":
+        if len(request.form['username']) > 2:
+            flash("Сообщение отправленно успешно", category='success')
+        else:
+            flash("Ошибка отправки", category='error')
+        # print(request.form)
+        # context = {
+        #     'username': request.form['username'],
+        #     'email': request.form['email'],
+        #     'message': request.form['message'],
+        # }
+        # return render_template("contact.html", ** context, title="Обратная связь", menu=menu)
+    return render_template("contact.html", title="Обратная связь", menu=menu)
+
+
+@app.route("/profile/<path:username>")
+def profile(username):
+    return f"Пользователь: {username} "
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST" and request.form['username'] == 'admin' and request.form['psw'] == '123456':
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    return render_template('login.html', title="Авторизация", menu=menu)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("page404.html", title="Страница не найдена", menu=menu)
 
 
 if __name__ == "__main__":
---
+++ projects/pj76/homework/fldbsite/fldbsite.py
@@ -0,0 +1,50 @@
+import sqlite3
+import os
+from flask import Flask, render_template, url_for, request, flash, session, redirect, g
+from FDataBase import FDataBase
+
+DATABASE = '/tmp/flsk.db'
+DEBUG = True
+SECRET_KEY = '428440dba36b4f3e6b6ad05b7bb90c7f53368506'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(DATABASE=os.path.join(app.root_path, 'flsk.db'))
+
+
+def connect_db():
+    con = sqlite3.connect(app.config['DATABASE'])
+    con.row_factory = sqlite3.Row
+    return con
+
+
+def create_db():
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+@app.route("/")
+def index():
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template("index.html", menu=dbase.get_menu())
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
