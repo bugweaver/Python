08afea3d7cd932a7118092db348484508f42dc5f
bugweaver <bugweaver@example.com>
2024-05-27 16:55:38 +0300

    pj77

---
+++ projects/pj77/flsite/fldbsite/fldbsite.py
@@ -0,0 +1,77 @@
+import sqlite3
+import os
+from flask import Flask, render_template, url_for, request, flash, session, redirect, g
+from FDataBase import FDataBase
+
+DATABASE = '/tmp/flsk.db'
+DEBUG = True
+SECRET_KEY = '428440dba36b4f3e6b6ad05b7bb90c7f53368506'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(DATABASE=os.path.join(app.root_path, 'flsk.db'))
+
+
+def connect_db():
+    con = sqlite3.connect(app.config['DATABASE'])
+    con.row_factory = sqlite3.Row
+    return con
+
+
+def create_db():
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+@app.route("/")
+def index():
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template("index.html", menu=dbase.get_menu(), posts=dbase.get_posts_annonce())
+
+
+@app.route('/add_post', methods=["POST", "GET"])
+def add_post():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.add_post(request.form['name'], request.form['post'], request.form["url"])
+            if not res:
+                flash("Ошибка добавления статьи", category='error')
+            else:
+                flash("Статья добавлена успешно", category='success')
+        else:
+            flash("Ошибка добавления статьи", category='error')
+
+    return render_template('add_post.html', menu=dbase.get_menu(), title="Добавление статьи")
+
+
+@app.route("/post/<alias>")
+def show_post(alias):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.get_post(alias)
+
+    return render_template("post.html", menu=dbase.get_menu(), title=title, post=post)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
