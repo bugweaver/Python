6a5c6b397e1bfccc87339702b584d3543adef722
bugweaver <you@example.com>
2024-06-15 13:48:13 +0300

    login+logout

--- Django/second_homework/cities_project/cities/views.py
+++ Django/second_homework/cities_project/cities/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Cities
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 
 
@@ -29,3 +29,24 @@ def signupuser(request):
         else:
             return render(request, 'cities/signupuser.html',
                           {'form': UserCreationForm(), 'error': "Пароли не совпадают"})
+
+
+def logoutuser(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('index')
+
+
+def loginuser(request):
+    if request.method == "GET":
+        return render(request, 'cities/loginuser.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'cities/loginuser.html',
+                          {'form': AuthenticationForm(), 'error': 'Неверные данные для входа'})
+
+        else:
+            login(request, user)
+            return redirect('index')
+
--- Django/second_homework/cities_project/cities_project/urls.py
+++ Django/second_homework/cities_project/cities_project/urls.py
@@ -8,7 +8,10 @@ from cities import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+
     path('signup/', views.signupuser, name="signupuser"),
+    path('logout/', views.logoutuser, name="logoutuser"),
+    path('login/', views.loginuser, name="loginuser"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
